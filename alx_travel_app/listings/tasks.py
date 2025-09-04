4c8c86d651bea14994b9a8bfe5ca4510d1fafd18
blessingebele <blessmart.com@gmail.com>
2025-09-04 23:43:46 +0100

    ready for deploymet

--- alx_travel_app/listings/tasks.py
+++ alx_travel_app/listings/tasks.py
@@ -1,12 +1,17 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from django.conf import settings
 
 @shared_task
 def send_booking_confirmation_email(to_email, booking_details):
     subject = "Booking Confirmation"
-    message = f"Hello, your booking was successful!\n\nDetails:\n{booking_details}"
-    from_email = settings.EMAIL_HOST_USER
-
+    message = (
+        f"Hello, your booking was successful!\n\n"
+        f"Booking Details:\n"
+        f"Booking ID: {booking_details.get('booking_id')}\n"
+        f"Destination: {booking_details.get('destination', 'N/A')}\n"
+        f"Date: {booking_details.get('date', 'N/A')}\n"
+    )
+    from_email = "webmaster@localhost"
     send_mail(subject, message, from_email, [to_email])
     return f"Email sent to {to_email}"
+
