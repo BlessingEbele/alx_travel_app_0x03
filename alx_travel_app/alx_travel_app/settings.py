a9895be49cf6c49f77b0202e6acb8e008793bc98
blessingebele <blessmart.com@gmail.com>
2025-09-04 22:55:11 +0100

    no

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -42,8 +42,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'blessmart.com@gmail.com'
-EMAIL_HOST_PASSWORD = 'cynthia95@'  # Use environment variables for security
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")   # Use environment variables for security
 
 
 # Application definition
